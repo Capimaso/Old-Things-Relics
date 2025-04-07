838d561c5e67853dfa2955eb021fe73052c76668
capimaso <tiago.cunha@aluno.fmpsc.edu.br>
2025-04-07 09:43:07 -0300

    Testando meu merge

---
+++ script.js
@@ -0,0 +1,6 @@
+let itens = [["Luger","luger.png",200],["M1911","m1911.png",150]]
+const logo = document.getElementById('logo')
+
+logo.addEventListener('mousedown',()=>{
+    window.location.href = 'home.html'
+})
