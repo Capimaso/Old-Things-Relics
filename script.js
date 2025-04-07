29fb94dcee4a5bd02ba21c899d5d7e39b9e5c12e
Capimaso <tiago.cunha@aluno.fmpsc.edu.br>
2025-04-07 13:27:23 -0300

    Tinha uma linha a mais no script, sei la porque

--- script.js
+++ script.js
@@ -5,6 +5,3 @@ const logo = document.getElementById('logo')
 logo.addEventListener('mousedown',()=>{
     window.location.href = 'home.html'
 })
-
-let itens = [["Luger","luger.png",200],["M1911","m1911.png",150]]
-
